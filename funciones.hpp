b4cb36d154b70182acacfe8aa5564b46bdd32e5d
David <Juandavides120@gmail.com>
2024-12-04 20:39:06 -0500

    Proyecto 1 sumarNumeros creado

---
+++ funciones.hpp
@@ -0,0 +1,7 @@
+#ifndef funciones_hpp
+#define funciones_hpp
+
+#include <stdio.h>
+void sumaN(int n);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "funciones.hpp"
+using namespace std;
+
+int main(){
+    int n;
+    cout <<"Ingrese un numero: ";
+    cin >>n;
+    sumaN(n);
+
+    return 0;
+}
