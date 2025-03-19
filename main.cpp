9a1fc5da579a41498643e0b960dfe45338a1efa9
DavidJuan2810 <Juandavides120@gmail.com>
2025-03-19 17:02:36 -0500

    otra edicion

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "funciones.hpp"
 using namespace std;
 
+
+
+
 int main(){
     int n;
-    cout <<"Ingrese el numero que desee: ";
+    cout <<"Ingrese el numero que dese: ";
     cin >>n;
     sumaN(n);
 
